cc8080fcd38563611221e79943776c1929dfbc18
Krishna <krishna.venu@georgebrown.ca>
2026-03-22 01:22:52 -0400

    Lab Week 10

---
+++ Lab07_101484996/exercise-1/myapp/routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+    res.send('POST received!');
+});
+
+module.exports = router;
